197698a6d0631ef9b93243f10fb03dadea10652c
jinlong18705 <jinlong.tan@advantech.com.cn>
2017-04-24 10:22:18 +0800

    modify lvds timing for rsb6410

--- drivers/video/backlight/pwm_bl.c
+++ drivers/video/backlight/pwm_bl.c
@@ -138,6 +138,8 @@ static const struct backlight_ops pwm_backlight_ops = {
 #ifdef CONFIG_ARCH_ADVANTECH
 int lvds_vcc_enable;
 int lvds_bkl_enable;
+int lvds-vcc-delay-value;
+int lvds-bkl-delay-value;
 enum of_gpio_flags lvds_vcc_flag;
 enum of_gpio_flags lvds_bkl_flag;
 
@@ -156,14 +158,14 @@ void enable_lcd_vdd_en(void)
 			gpio_direction_output(lvds_vcc_enable, lvds_vcc_flag);
 	}
 
-	mdelay(250);
+	mdelay(lvds-vcc-delay-value);
 }
 
 void enable_ldb_bkl_pwm(void)
 {
 	int ret;
 
-	mdelay(20);
+	mdelay(lvds-bkl-delay-value);
 	
         if (lvds_bkl_enable > 0)
         {
@@ -232,6 +234,16 @@ static int pwm_backlight_parse_dt(struct device *dev,
 #ifdef CONFIG_ARCH_ADVANTECH
 	lvds_vcc_enable = of_get_named_gpio_flags(node, "lvds-vcc-enable", 0, &lvds_vcc_flag);
 	lvds_bkl_enable = of_get_named_gpio_flags(node, "lvds-bkl-enable", 0, &lvds_bkl_flag);
+	ret = of_property_read_u32(node,"lvds-vcc-delay-time",&lvds-vcc-delay-value);
+	if (ret < 0)
+	{
+		lvds-vcc-delay-value = 250;
+	}
+	ret = of_property_read_u32(node,"lvds-bkl-delay-time",&lvds-blk-delay-value);
+	if (ret < 0)
+	{
+		lvds-blk-delay-value = 20;
+	}
 #endif
 
 	return 0;
