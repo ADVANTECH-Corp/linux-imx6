5bbbd8d09a216ebc2323af8a2261f37cff595b5c
clayderhua <clayderhua@gmail.com>
2020-11-02 22:33:10 +0800

    Modify EMMC speed ESDHC_FLAG_HS400 to ESDHC_FLAG_HS200

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -213,7 +213,7 @@ static struct esdhc_soc_data usdhc_imx6ull_data = {
 static struct esdhc_soc_data usdhc_imx7d_data = {
 	.flags = ESDHC_FLAG_USDHC | ESDHC_FLAG_STD_TUNING
 			| ESDHC_FLAG_HAVE_CAP1 | ESDHC_FLAG_HS200
-			| ESDHC_FLAG_HS400 | ESDHC_FLAG_STATE_LOST_IN_LPMODE,
+			| ESDHC_FLAG_STATE_LOST_IN_LPMODE,
 };
 
 struct pltfm_imx_data {
